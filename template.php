de2de5bf39d4ce267dd0ae440b572a392913dbaf
prathmesh36 <prathmesh36@yahoo.com>
2018-07-21 17:28:54 +0530

    Template File

---
+++ template.php
@@ -0,0 +1,139 @@
+<?php
+  include'all.php';
+?>
+<style>
+  .bg-title{
+    background: rgba(60,141,188,1);
+    background: -moz-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    background: -webkit-gradient(left top, right top, color-stop(0%, rgba(60,141,188,1)), color-stop(0%, rgba(60,141,188,1)), color-stop(59%, rgba(60,141,188,1)), color-stop(97%, rgba(80,161,208,1)), color-stop(100%, rgba(80,161,208,1)));
+    background: -webkit-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    background: -o-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    background: -ms-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    background: linear-gradient(to right, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#dd4b39', endColorstr='#f16f5c', GradientType=1 );
+  }
+
+  .bg-title2{
+    background: rgba(60,141,188,1);
+    background: -moz-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 27%, rgba(80,161,208,1) 90%, rgba(80,161,208,1) 100%);
+    background: -webkit-gradient(left top, right top, color-stop(0%, rgba(60,141,188,1)), color-stop(0%, rgba(60,141,188,1)), color-stop(27%, rgba(60,141,188,1)), color-stop(90%, rgba(80,161,208,1)), color-stop(100%, rgba(80,161,208,1)));
+    background: -webkit-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 27%, rgba(80,161,208,1) 90%, rgba(80,161,208,1) 100%);
+    background: -o-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 27%, rgba(80,161,208,1) 90%, rgba(80,161,208,1) 100%);
+    background: -ms-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 27%, rgba(80,161,208,1) 90%, rgba(80,161,208,1) 100%);
+  }
+
+  @media only screen and (max-width: 990px) {
+    .pm, .pro-box{
+      border-bottom:solid 1px silver;
+      padding-bottom: 10px;
+    }
+
+    .boxzol{
+      margin-bottom:20px;
+    }
+
+    .forum{
+      margin-left:5px;
+      margin-right:5px;
+    }
+
+    .forum2{
+      margin-left:5px;
+      margin-right:5px;
+    }
+
+    .topinfo{
+      margin-left:2px;
+      margin-right:2px;
+    }
+  }
+  @media only screen and (min-width: 990px) {
+    .pm{
+      border-right:solid 1px silver;
+      border-left:solid 1px silver;
+    }
+
+    .boxzol{
+      margin-bottom:10px;
+    }
+
+    .topinfo{
+      margin-left:10px;
+      margin-right:10px;
+
+    }
+
+    .forum2{
+      padding-left: 0px;
+    }
+  }
+</style>
+
+<div class="content-wrapper" style="min-height: 669px;">
+    <div class="row" style="background-color:#ecf0f5;height:5px;"></div>
+    <div class="row topinfo">
+      <div class="col-md-12" style="padding-left:10px;padding-right:10px;">
+        <div class="box boxzol" style="">
+          <div class="box-header with-border bg-title2" style="">
+            <h2 class="box-title" style="font-size:22px;color:white;">Admin Guy's Profile</h2>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <div class="row">
+              <!---ADD your Code here>
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- ./box-body -->
+          <div class="box-footer">
+            <div class="row">
+              <div class="col-sm-3 col-xs-6">
+                <div class="description-block border-right">
+                  <h5 class="description-header text-green"><i class="fa fa-user"></i> &nbsp;Followers</h5>
+                  <span class="description-text">
+                    15<br><a href="#" id="followers" data-toggle="modal" data-target=".bd-example-modal-lg" onclick="modalfunc(1);">See all</a>                  </span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+              <div class="col-sm-3 col-xs-6">
+                <div class="description-block border-right">
+                  <h5 class="description-header text-blue"><i class="fa fa-user"></i> &nbsp;Following</h5>
+                  <span class="description-text">
+                    67<br><a href="#" id="followers" data-toggle="modal" data-target=".bd-example-modal-lg2" onclick="modalfunc(2);">See all</a>                  </span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+              <div class="col-sm-3 col-xs-6">
+                <div class="description-block border-right">
+                  <h5 class="description-header text-red"><i class="fa fa-newspaper-o"></i> &nbsp;Forum Post</h5>
+                  <span class="description-text">
+                    0                  </span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+              <div class="col-sm-3 col-xs-6">
+                <div class="description-block">
+                  <h5 class="description-header text-yellow"><i class="fa fa-cart-plus"></i> &nbsp;User Type</h5>
+                  <span class="description-text">
+                    Patient                  </span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.box-footer -->
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</div>
+<?php
+include'footer.php';
+
+?>
