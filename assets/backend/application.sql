e34d5e01033197150d51cbc58f32927ff24374cd
Priyanka <priyankanarwani97@gmail.com>
2018-07-21 03:26:35 +0100

    application db

--- assets/backend/application.sql
+++ assets/backend/application.sql
@@ -1,62 +1,22 @@
--- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 21, 2018 at 06:43 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `educom`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `application`
---
-
 CREATE TABLE `application` (
   `aid` int(20) NOT NULL,
   `id` int(20) NOT NULL,
-  `Status` varchar(20) NOT NULL
+  `Status` varchar(20) NOT NULL,
+  `institute` varchar(50) NOT NULL,
+  `typeedu` varchar(50) NOT NULL,
+  `result` float NOT NULL,
+  `income` float NOT NULL,
+  `year` int(11) NOT NULL,
+  `occupation` varchar(50) NOT NULL,
+  `did` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `application`
 --
 
---
--- Indexes for table `application`
---
-ALTER TABLE `application`
-  ADD PRIMARY KEY (`aid`),
-  ADD UNIQUE KEY `id` (`aid`),
-  ADD UNIQUE KEY `Did` (`aid`);
+INSERT INTO `application` (`aid`, `id`, `Status`, `institute`, `typeedu`, `result`, `income`, `year`, `occupation`, `did`) VALUES
+(1, 1, '1', 'Ruia', '12th', 89, 10000, 2014, 'Farmer', 1),
+(3, 1, '1', 'Ruia', '11th', 80, 12000, 2013, 'Farmer', 2);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `application`
---
-ALTER TABLE `application`
-  MODIFY `aid` int(20) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
