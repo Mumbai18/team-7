fc2ace6f67d8ee47190c6facb76bade008c2d303
Gurpreet Kaur <gurpreet.saimy@gmail.com>
2018-07-21 12:17:05 +0530

    Personal Details 2

--- personal_details.php
+++ personal_details.php
@@ -1,8 +1,7 @@
 <?php
-  include 'all.php';
+  include'all.php';
 ?>
-
-  <script type="text/javascript">
+<script type="text/javascript">
     var     head = document.getElementsByTagName('head')[0],
             link1 = document.createElement('link'),
             link2 = document.createElement('link');
@@ -13,64 +12,87 @@
     head.appendChild(link1);
     head.appendChild(link2);
   </script>
+<style>
+  .bg-title{
+    background: rgba(60,141,188,1);
+    background: -moz-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    background: -webkit-gradient(left top, right top, color-stop(0%, rgba(60,141,188,1)), color-stop(0%, rgba(60,141,188,1)), color-stop(59%, rgba(60,141,188,1)), color-stop(97%, rgba(80,161,208,1)), color-stop(100%, rgba(80,161,208,1)));
+    background: -webkit-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    background: -o-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    background: -ms-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    background: linear-gradient(to right, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 59%, rgba(80,161,208,1) 97%, rgba(80,161,208,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#dd4b39', endColorstr='#f16f5c', GradientType=1 );
+  }
 
-  <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper" style="background-image:url('images/backgrounds/1.jpg');padding-bottom:2%;background-repeat: no-repeat;background-size: cover;" >
-      <div class="row" style="margin:0 3%;">
-        <div class="modal fade bd-example-modal-lg error" id="error" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-lg1">
-            <div class="modal-content" style="width:100%:">
-              <div class="modal-header">
-                <h3 class="modal-title" >Login Error</h3>
-              </div>
-              <div class="modal-body xsfonts" style="" id="folist2">
-                Invalid Email ID or Password
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" style="font-size:15px;" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
+  .bg-title2{
+    background: rgba(60,141,188,1);
+    background: -moz-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 27%, rgba(80,161,208,1) 90%, rgba(80,161,208,1) 100%);
+    background: -webkit-gradient(left top, right top, color-stop(0%, rgba(60,141,188,1)), color-stop(0%, rgba(60,141,188,1)), color-stop(27%, rgba(60,141,188,1)), color-stop(90%, rgba(80,161,208,1)), color-stop(100%, rgba(80,161,208,1)));
+    background: -webkit-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 27%, rgba(80,161,208,1) 90%, rgba(80,161,208,1) 100%);
+    background: -o-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 27%, rgba(80,161,208,1) 90%, rgba(80,161,208,1) 100%);
+    background: -ms-linear-gradient(left, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 0%, rgba(60,141,188,1) 27%, rgba(80,161,208,1) 90%, rgba(80,161,208,1) 100%);
+  }
 
-        <div class="modal fade bd-example-modal-lg error1" id="error1" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-lg1">
-            <div class="modal-content" style="width:100%:">
-              <div class="modal-header">
-                <h3 class="modal-title" >Login Error</h3>
-              </div>
-              <div class="modal-body xsfonts" style="" id="folist2">
-                Invalid Details
-              </div>
-              <div class="modal-footer">
-               <button type="button" class="btn btn-default" style="font-size:15px;" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="modal fade bd-example-modal-lg error2" id="error2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-lg1">
-            <div class="modal-content" style="width:100%:">
-              <div class="modal-header">
-                <h3 class="modal-title" >Login Error</h3>
-              </div>
-              <div class="modal-body xsfonts" style="" id="folist2">
-               Enter Details
-              </div>
-              <div class="modal-footer">
-               <button type="button" class="btn btn-default" style="font-size:15px;" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <?php
-          if(!isset($_SESSION['user_id']) && empty($_SESSION['user_id']))
-          {
-        ?>
+  @media only screen and (max-width: 990px) {
+    .pm, .pro-box{
+      border-bottom:solid 1px silver;
+      padding-bottom: 10px;
+    }
 
-        <div class="col-sm-5">
+    .boxzol{
+      margin-bottom:20px;
+    }
+
+    .forum{
+      margin-left:5px;
+      margin-right:5px;
+    }
+
+    .forum2{
+      margin-left:5px;
+      margin-right:5px;
+    }
+
+    .topinfo{
+      margin-left:2px;
+      margin-right:2px;
+    }
+  }
+  @media only screen and (min-width: 990px) {
+    .pm{
+      border-right:solid 1px silver;
+      border-left:solid 1px silver;
+    }
+
+    .boxzol{
+      margin-bottom:10px;
+    }
+
+    .topinfo{
+      margin-left:10px;
+      margin-right:10px;
+
+    }
+
+    .forum2{
+      padding-left: 0px;
+    }
+  }
+</style>
+
+<div class="content-wrapper" style="min-height: 669px;">
+    <div class="row" style="background-color:#ecf0f5;height:5px;"></div>
+    <div class="row topinfo">
+      <div class="col-md-12" style="padding-left:10px;padding-right:10px;">
+        <div class="box boxzol" style="">
+          <div class="box-header with-border bg-title2" style="">
+            <h2 class="box-title" style="font-size:22px;color:white;">Personal Details</h2>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <div class="row">
+              <!---ADD your Code here-->
+				<div class="col-sm-5">
           <div class="form-box">
             <div class="form-top">
               <div class="form-top-left">
@@ -96,39 +118,22 @@
             </div>
           </div>
         </div>
-        
-           
-          </sdiv>
+            </div>
+            <!-- /.row -->
+			</div>
+          <!-- ./box-body -->
+          <!-- /.box-footer -->
+			</div>
+        <!-- /.box -->
+		</div>
+      <!-- /.col -->
+	  </div>
+    <!-- /.row -->
+	</div>
+</div>
+<?php
+include'footer.php';
 
-        </div>
-        <?php
-          }
-          else
-          {
-            echo '<script type="text/javascript">';
-            echo 'window.location.href="profile.php";';
-            echo '</script>';
-          }
-        ?>
-    </div>
-  </div>
-  <!-- /.content-wrapper -->
-  <?php
-    include 'footer.php';
-  ?>
-  <script>
-    //shortcut for $(document).ready
-    var hash = window.location.hash;
-    if (hash.substring(1) == 'error')
-    {
-        $(hash).modal('toggle');
-    }
-    if(hash.substring(1) == 'error1')
-    {
-        $(hash).modal('toggle');
-    }
-    if(hash.substring(1) == 'error2')
-    {
-        $(hash).modal('toggle');
-    }
-  </script>
+?>
+
+	
